d97ce51fd4afaca39528b0ddd379160647281ac9
nas <nas.silver.bullet@gmail.com>
2019-11-09 22:05:28 +0900

    refactor controller

--- app/interfaces/user_handler.go
+++ app/interfaces/user_handler.go
@@ -29,13 +29,14 @@ func NewUserHandler(logger Logger, validator Validator, sqlHandler SQLHandler) *
 }
 
 func (uh *UserHandler) List(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
 	uh.Logger.Infof("%s: %s => Start user handler's List", r.Method, r.URL.Path)
 
 	us, err := uh.UserInteractor.List()
 	if err != nil {
 		uh.Logger.Error(err)
 
-		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(500)
 		json.NewEncoder(w).Encode(err)
 
@@ -44,13 +45,14 @@ func (uh *UserHandler) List(w http.ResponseWriter, r *http.Request) {
 
 	uh.Logger.Infof("%s: %s => Success find %d users", r.Method, r.URL.Path, len(us))
 
-	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(us)
 
 	uh.Logger.Infof("%s: %s => Finished user handler's List", r.Method, r.URL.Path)
 }
 
 func (uh *UserHandler) Get(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
 	uh.Logger.Infof("%s: %s => Start user handler's Get", r.Method, r.URL.Path)
 
 	_, idStr := path.Split(r.URL.Path)
@@ -58,7 +60,6 @@ func (uh *UserHandler) Get(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		uh.Logger.Error(err)
 
-		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(400)
 		json.NewEncoder(w).Encode(err)
 
@@ -71,7 +72,6 @@ func (uh *UserHandler) Get(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		uh.Logger.Error(err)
 
-		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(500)
 		json.NewEncoder(w).Encode(err)
 
@@ -80,20 +80,20 @@ func (uh *UserHandler) Get(w http.ResponseWriter, r *http.Request) {
 
 	uh.Logger.Infof(`%s: %s => Success get user >> {"id":%d,"name":%s}`, r.Method, r.URL.Path, u.ID, u.Name)
 
-	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(u)
 
 	uh.Logger.Infof("%s: %s => Finished user handler's Get", r.Method, r.URL.Path)
 }
 
 func (uh *UserHandler) Create(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
 	uh.Logger.Infof("%s: %s => Start user handler's Create", r.Method, r.URL.Path)
 
 	var u *entities.User
 	if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
 		uh.Logger.Error(err)
 
-		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(400)
 		json.NewEncoder(w).Encode(err)
 
@@ -103,7 +103,6 @@ func (uh *UserHandler) Create(w http.ResponseWriter, r *http.Request) {
 	if err := uh.Validator.Struct(u); err != nil {
 		uh.Logger.Error(err)
 
-		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(400)
 		json.NewEncoder(w).Encode(err)
 
@@ -116,7 +115,6 @@ func (uh *UserHandler) Create(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		uh.Logger.Error(err)
 
-		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(500)
 		json.NewEncoder(w).Encode(err)
 
@@ -125,7 +123,6 @@ func (uh *UserHandler) Create(w http.ResponseWriter, r *http.Request) {
 
 	uh.Logger.Infof(`%s: %s => Success create user >> id %d`, r.Method, r.URL.Path, id)
 
-	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(id)
 
 	uh.Logger.Infof("%s: %s => Finished user handler's Create", r.Method, r.URL.Path)
